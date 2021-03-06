ea0e710e9c9e285bed090d809c766fc24fa23f06
unknown <jerome.lavoue@umontreal.ca>
2021-03-05 19:07:10 -0500

    Finalizing the EN DB

--- EnglishDB/data_preparation.R
+++ EnglishDB/data_preparation.R
@@ -38,17 +38,79 @@
 
 ######### treating the standardized variables      
       
+      # names of the reference tables
+      
+      stdvariable_names <- myvariables$short_en[ myvariables$ref_file==1 & !is.na(myvariables$ref_file) ]
+      
+      ### initialising the list of tables
+      
+      ref.tables <- vector("list", length = length(stdvariable_names))
+      
+      ##creating a list the tables
+      
+      ## the link variable is always the same as the short_en name in the main dataframe
+      
+      ## first table (ref ID)
+      
       i <- 1
       
-      mycategory <- Allmydata[[i+1]]
+      tempdata <- Allmydata[[i+1]]
 
-      names(mycategory)
+      names(tempdata) <- c( "Reference" , "description" )
+      
+      ref.tables[[i]] <- tempdata
+      
+      ## other tables
+      
+      for (i in 2:length(stdvariable_names)) {
+      
+      tempdata <- Allmydata[[i+1]]
+      
+        #only the english
+      
+      tempdata <-  tempdata[ , c(1,3) ]
+      
+      names(tempdata) <- c( stdvariable_names[i] , "Description" )
+      
+      ref.tables[[i]] <- tempdata
+      
+      }
+      
+      ## naming the elements
+      
+      names(ref.tables) <- stdvariable_names
             
 ########## saving relevant RDS R files
       
+      SilicaDB_en <- mydata
       
+      saveRDS( SilicaDB_en , "./EnglishDB/RDS_objects/SilicaDB_en.RDS")
       
+      SilicaRefTables_en <- ref.tables
       
+      saveRDS( SilicaRefTables_en , "./EnglishDB/RDS_objects/SilicaRefTables_en.RDS")
       
-
+      SilicaVarList_en <- myvariables
+      
+      saveRDS( SilicaVarList_en , "./EnglishDB/RDS_objects/SilicaVarList_en.RDS")
             
+      
+########### saving data as CSV
+      
+      write.csv( SilicaDB_en ,  "./EnglishDB/CSV_files/SilicaDB_en.csv"  , row.names = FALSE , quote = TRUE )
+      
+      write.csv( SilicaVarList_en ,  "./EnglishDB/CSV_files/SilicaVarList_en.csv"  , row.names = FALSE , quote = TRUE )
+      
+      for (i in 1:length(stdvariable_names)) { 
+        
+        write.csv( SilicaRefTables_en[[i]] ,  paste("./EnglishDB/CSV_files/",
+                                                    stdvariable_names[i],
+                                                      ".csv", sep="")  
+                   , row.names = FALSE , quote = TRUE )
+        
+        }
+      
+############ clearing thr R environment  
+      
+      rm(list = ls())
+      
